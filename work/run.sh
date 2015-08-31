78a74f7c3c5b186c96ad9d49e0c1ed1a427708b4
Alex Davis <alex2006hw@gmail.com>
2015-08-31 00:43:55 -0700

    update

--- work/run.sh
+++ work/run.sh
@@ -6,6 +6,7 @@ docker run --name webserver -d \
   -v `pwd`/log:/var/log/nginx \
   -v `pwd`/upload:/var/upload \
   -p 3000:3000 -p 8000:80 -p 8433:443 \
+  -e APP='message.2bees.com' \
   -t pointlook/webserver
 
 docker exec -it webserver bash
